8ee5e274bb1eb70ddc3ff040f35157dfd1e6b7a1
JairamDesik <jairamdesik@gmail.com>
2016-09-28 21:00:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
+
 //Change the main-text
 var element = document.getElementById('main-text');
 element.innerHTML = 'It is FUN';
 
+//Move the Image
+var img = document.getElementById('img');
+img.onclick = functon(){
+    img.style.marginLeft = '200px';
+}
