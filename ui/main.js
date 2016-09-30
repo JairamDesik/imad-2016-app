2aa55d913bcfe43b511795d5acbe229c7c7fd54c
JairamDesik <jairamdesik@gmail.com>
2016-09-30 08:56:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,10 +18,8 @@ element.innerHTML = 'It is FUN';
 //Counter code
 var button = document.getElementById('countre');
 var counter = 0;
-window.onload=function(){
-    button.onclick=function(){
-        counter = counter + 1;
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-    };
+button.onclick=function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
