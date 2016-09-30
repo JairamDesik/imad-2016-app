096f1fc0a66c9bea57b619a3fab28af1104070c5
JairamDesik <jairamdesik@gmail.com>
2016-09-30 13:08:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,16 +37,32 @@ button.onclick = function () {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit-name');
 submit.onclick = function(){
-    var names = ['name1','name2','name3'];
-    var list = '';
-     for(var i=0; i<names.length; i++){
-         list+= '<li>' + names[i] + '</li>'; 
-      }
-     var ul = document.getElementById('namelist');  
-     ul.innerHTML = list;
-
+    //Create a request Object
+    var request = new XMLHttpRequest();
+    //Captuer the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+             //Take some action
+            if(request.status === 200){
+                //Capture a list of names and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list+= '<li>' + names[i] + '</li>'; 
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            
+            }
+        }
+    };
+    //Make the Request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://jairamdesik.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
+
