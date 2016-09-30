8887e519f99295c1ccdcc9e7a52869e67653430d
JairamDesik <jairamdesik@gmail.com>
2016-09-30 08:54:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,27 @@
+console.log('Loaded!');
 
+//Change the main-text
+var element = document.getElementById('main-text');
+element.innerHTML = 'It is FUN';
+
+//Move the Image
+//var img = document.getElementById('imad');
+//var marginLeft = 0;
+//function moveRight(){
+//    marginLeft= marginLeft+10;
+//    img.style.marginLeft= marginLeft+'px';
+//}
+//img.onclick=function(){
+//    var interval=setInterval(moveRight,10);
+//};
 
 //Counter code
 var button = document.getElementById('countre');
 var counter = 0;
-button.onclick=function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+window.onload=function(){
+    button.onclick=function(){
+        counter = counter + 1;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+    };
 };
