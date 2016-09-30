2ce6a398f9612b4ce9860fa86114e1792be69ef9
JairamDesik <jairamdesik@gmail.com>
2016-09-30 10:04:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,9 +17,22 @@ element.innerHTML = 'It is FUN';
 
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Create a request object
+    var request = new XMLHttpRequest();
+    //Captuer the response and store it in a variable
+    request.onreadyStatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.Status === 200){
+                var counter=request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+   //Make a request
+   request.open('GET','http://jairamdesik.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
+
