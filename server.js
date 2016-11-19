ebd36959f1f08825d0d7f418253fc4d3b0b7c7d7
JairamDesik <jairamdesik@gmail.com>
2016-11-19 10:29:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,18 +122,18 @@ app.get('/submit-name',function(req,res){
     //JSON:Javascript Object Notation
     res.send(JSON.stringify(names));
 });
+
 app.get('article/:articleName',function (req,res){
   pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + "'",function(err,result){
       if(err){
           res.status(500).send(err.toString());
       }else{
-          if(result.rows.length ===0){
-              req.status(404).send('Article not found');
+          if(result.rows.length === 0){
+              res.status(404).send('Article not found');
           }else{
                var articleData = result.rows[0];
                res.send(createTemplate(articleData));
           }
-         
       }
   });
 });
