b9fe95450cfe708a55f87a9375993fcc0606ffbc
JairamDesik <jairamdesik@gmail.com>
2016-11-19 10:17:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,8 +127,13 @@ app.get('article/:articleName',function (req,res){
       if(err){
           res.status(500).send(err.toString());
       }else{
-          var articleData = result.rows[0];
-          res.send(createTemplate(articleData));
+          if(result.rows.length ===0){
+              req.status(404).send('Article not found');
+          }else{
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+          }
+         
       }
   });
 });
